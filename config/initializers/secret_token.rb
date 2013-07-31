952bf5235413069b942badc625938e91d39a8a7c
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-07-31 14:44:05 +0100

    Initial commit of Rails project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JustOneThing::Application.config.secret_key_base = '8388d0d38ca1741a140c548f32195f9e52b735378d123dada55cc746cc0e8ea5af483e1120928c886f6794bdc6a71882cf79a4d7225919cb89bdffce2d5e5b56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JustOneThing::Application.config.session_store :cookie_store, key: '_just-one-thing_session'
