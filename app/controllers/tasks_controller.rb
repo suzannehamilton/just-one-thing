f1d4e0990b6908774fa6df00eb56e704f513c332
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-07-31 15:34:31 +0100

    Add create method to controller for saving a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,5 +3,14 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
+
+    @task.save
+    redirect_to @task
   end
+
+  private
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
