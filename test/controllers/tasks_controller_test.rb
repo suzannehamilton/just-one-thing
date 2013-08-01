e5e4c887b03efb18d3d946fa3bf17b1db96035da
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-08-01 23:27:03 +0100

    Add test for task controller index action

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:tasks)
+  end
 end
