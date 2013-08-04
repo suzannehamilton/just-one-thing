86e539706cc27944e3965a98646d46533fc61bac
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-08-05 00:33:14 +0100

    Test that a task has no children or parents by default

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -11,4 +11,14 @@ class TaskTest < ActiveSupport::TestCase
     task.save
     refute task.completed
   end
+
+  test "task has no parent by default" do
+    task = Task.new
+    assert_nil task.parent
+  end
+
+  test "task has no children by default" do
+    task = Task.new
+    assert task.children.size == 0
+  end
 end
