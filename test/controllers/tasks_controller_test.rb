e61fd66f8b5fe119b62f9ef18a8ad1e24eff8eb8
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-08-17 13:50:56 +0100

    Add test for task endpoint which allows you to create a new task

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "index should get the full list of tasks" do
     get :index
     assert_response :success
     assert_not_nil assigns(:tasks)
+  end
+
+  test "new should be a valid page" do
+    get :new
+    assert_response :success
   end
 end
