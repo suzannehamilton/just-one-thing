641962136a80158b1b9561522ecf666dba622818
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2013-08-18 17:37:50 +0100

    Generate home controller as a starting point for adding devise user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 JustOneThing::Application.routes.draw do
+  get "home/index"
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
